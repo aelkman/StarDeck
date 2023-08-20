0b55a29958fb457694fb84883d053204e9e07e2f
unknown <aelkman@gmail.com>
2023-08-19 19:27:54 -0700

    fix the card hover effect. hard coded result for optimized solution

--- Assets/ExpandMouseHover.cs
+++ Assets/ExpandMouseHover.cs
@@ -5,18 +5,20 @@ using UnityEngine;
 
 public class ExpandMouseHover : MonoBehaviour
 {
-    public float expandSize = 0.015f;
+    public float expandSize;
     private bool coroutineAllowed;
     private Vector3 originalScale;
     private Vector3 expandedScale;
+
     Coroutine start;
     Coroutine stop;
     // Start is called before the first frame update
     void Start()
     {
+        expandedScale = new Vector3(3.74f, 4.81f, 0.00f);
         coroutineAllowed = true;
         originalScale = transform.localScale;
-        expandedScale = new Vector3(originalScale.x + (expandSize * 10), originalScale.y + (expandSize * 10));
+        // expandedScale = new Vector3(originalScale.x + (expandSize * 10), originalScale.y + (expandSize * 10));
     }
 
     private void OnMouseExit() {
@@ -38,23 +40,30 @@ public class ExpandMouseHover : MonoBehaviour
     private IEnumerator HoverPulse() {
         coroutineAllowed = false;
         Vector3 newScale = originalScale;
+        Debug.Log("originalScale: " + originalScale);
         for (float i = 0f; i <= 1f; i+= 0.1f) {
             transform.localScale = new Vector3(
                 (Mathf.Lerp(newScale.x, newScale.x + expandSize, Mathf.SmoothStep(0f, 1f, i))),
-                (Mathf.Lerp(newScale.y, newScale.y + expandSize, Mathf.SmoothStep(0f, 1f, i)))
+                (Mathf.Lerp(newScale.y, newScale.y + expandSize, Mathf.SmoothStep(0f, 1f, i))),
+                0
             );
+            Debug.Log(transform.localScale);
             newScale = transform.localScale;
             yield return new WaitForSeconds(0.015f);
         }
+        Debug.Log("finalScale: " + transform.localScale);
     }
 
     private IEnumerator ExitShrink() {
         transform.localScale = expandedScale;
+        Debug.Log("expandedScale: " +  expandedScale);
         for (float i = 0f; i <= 1f; i+= 0.1f) {
             transform.localScale = new Vector3(
-                (Mathf.Lerp(expandedScale.x, expandedScale.x - expandSize, Mathf.SmoothStep(0f, 1f, i))),
-                (Mathf.Lerp(expandedScale.y, expandedScale.y - expandSize, Mathf.SmoothStep(0f, 1f, i)))
+                (Mathf.Lerp(transform.localScale.x, transform.localScale.x - expandSize, Mathf.SmoothStep(0f, 1f, i))),
+                (Mathf.Lerp(transform.localScale.y, transform.localScale.y - expandSize, Mathf.SmoothStep(0f, 1f, i))),
+                0
             );
+            Debug.Log(transform.localScale);
             yield return new WaitForSeconds(0.015f);
         }
         coroutineAllowed = true;
