e12d63c3b0f3ad68c0b50d476b0dbcc74d98686b
unknown <aelkman@gmail.com>
2023-08-24 16:33:40 -0700

    added rob as enemy, fixed player physics movement to use dynamic body for enable physics and correct collision event recognition

--- Assets/PlayerController.cs
+++ Assets/PlayerController.cs
@@ -42,80 +42,14 @@ public class PlayerController : MonoBehaviour
     }
 
     private void FixedUpdate() {
-        if(canMove) {
-            // If movement input is not 0, try to move
-            if(movementInput != Vector2.zero){
-                
-                bool success = TryMove(movementInput);
-
-                if(!success) {
-                    success = TryMove(new Vector2(movementInput.x, 0));
-                }
-
-                if(!success) {
-                    success = TryMove(new Vector2(0, movementInput.y));
-                }
-
-                // this.transform.Translate(new Vector2(movementInput.x, 0), Space.World);
-            
-                // animator.SetBool("isMoving", success);
-            } else {
-                // animator.SetBool("isMoving", false);
-            }
-
-            // Set direction of sprite to movement direction
-            // if(movementInput.x < 0) {
-            //     spriteRenderer.flipX = true;
-            // } else if (movementInput.x > 0) {
-            //     spriteRenderer.flipX = false;
-            // }
+        if (canMove) {
+            rb.velocity = movementInput * moveSpeed;
         }
-    }
-
-    private bool TryMove(Vector2 direction) {
-        if(direction != Vector2.zero) {
-            // Check for potential collisions
-            int count = rb.Cast(
-                direction, // X and Y values between -1 and 1 that represent the direction from the body to look for collisions
-                movementFilter, // The settings that determine where a collision can occur on such as layers to collide with
-                castCollisions, // List of collisions to store the found collisions into after the Cast is finished
-                moveSpeed * Time.fixedDeltaTime + collisionOffset); // The amount to cast equal to the movement plus an offset
-
-            if(count == 0){
-                rb.MovePosition(rb.position + direction * moveSpeed * Time.fixedDeltaTime);
-                return true;
-            } else {
-                return false;
-            }
-        } else {
-            // Can't move if there's no direction to move in
-            return false;
-        }
-        
     }
 
     void OnMove(InputValue movementValue) {
         movementInput = movementValue.Get<Vector2>();
     }
-
-    // void OnFire() {
-    //     animator.SetTrigger("swordAttack");
-    // }
-
-    // public void SwordAttack() {
-    //     LockMovement();
-
-    //     if(spriteRenderer.flipX == true){
-    //         swordAttack.AttackLeft();
-    //     } else {
-    //         swordAttack.AttackRight();
-    //     }
-    // }
-
-    // public void EndSwordAttack() {
-    //     UnlockMovement();
-    //     swordAttack.StopAttack();
-    // }
 
     public void LockMovement() {
         canMove = false;
