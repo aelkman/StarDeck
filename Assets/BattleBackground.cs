2e6423f2f4d36155c3eea721a88f3835b95b75ff
unknown <aelkman@gmail.com>
2023-09-27 00:43:33 -0700

    work

--- Assets/BattleBackground.cs
+++ Assets/BattleBackground.cs
@@ -5,12 +5,12 @@ using UnityEngine.UI;
 
 public class BattleBackground : MonoBehaviour
 {
-    public Image image;
+    public Sprite image;
     // Start is called before the first frame update
     void Start()
     {
         var images = Resources.LoadAll<Sprite>("Battle Backgrounds");
-        image.sprite = images[Random.Range(0, images.Length)];
+        image = images[Random.Range(0, images.Length)];
     }
 
     // Update is called once per frame
